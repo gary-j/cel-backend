b897e2449b1388d5eb4ca113bb86c589d49b1bcf
gary-j <gary.j@live.fr>
2022-05-24 02:32:23 +0200

    isAuthenticated middleware ok, route verify_get ok, next: blacklisted token db

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -2,37 +2,57 @@ const expressjwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.TOKEN_SECRET;
 
-const isAuthenticated = expressjwt({
-  secret: process.env.TOKEN_SECRET,
-  algorithms: ['HS256'],
-  requestProperty: 'payload',
-  getToken: getTokenFromHeaders,
-});
+const isAuthenticated = (req, res, next) => {
+  console.log('  *** isAuthenticate /verify ***');
 
-// Function used to extracts the JWT token from the request's 'Authorization' Headers
-// Authorization: Bearer <JWT>
-
-function getTokenFromHeaders(req) {
-  // Check if the token is available on the request Headers
-  console.log('Auth in progress');
+  const token = req.headers.authorization?.split(' ')[1];
 
   try {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Bearer'
-    ) {
-      // Get the encoded token string and return it
-      const token = req.headers.authorization.split(' ')[1];
-      console.log(token, 'The TOKEN');
-
-      return token;
+    if (!req.headers.authorization) {
+      console.log('  *** isAuthenticate : pas de token/user dans headers ***');
+      res
+        .status(401)
+        .json({ authenticated: false, message: 'error, aucun token/user' });
     }
-    console.log('<><<<<<<<<<<<     No TOKEN');
-    return null;
-  } catch (err) {
-    console.log(err, 'error de getToken JWT MIDDLEWARE');
+    if (token) {
+      console.log('*** LE TOKEN à vérifier *** : ', token);
+      // 1. VERIFIER LA TOKEN BLACKLIST EN DB
+      // if blacklistedToken === token
+      // decoded.authenticated = false;
+      // res.json({ authenticated: false, data: 'error , unauthorized' });
+      //
+      // 2.
+      jwt.verify(token, SECRET, function (error, decoded) {
+        //
+        console.log('*** error jwt.verify ***');
+        //
+        if (error?.name === 'TokenExpiredError' || error !== null) {
+          console.log('*** Il y a une erreur *** : ', error);
+          res
+            .status(401)
+            .json({ authenticated: false, message: 'Token invalide' });
+        }
+        if (decoded) {
+          console.log('decoded ! : ', decoded);
+
+          // 2. J'ajoute la clé 'authenticated':true
+          // j'ai déjà vérifier en DB la token blacklist
+
+          decoded.authenticated = true;
+          // delete decoded.isAdmin;
+
+          // 3. J'ajoute l'objet decoded à la requete
+          // pour y avoir acces dans la route suivante - 'Next()'
+
+          req.decoded = decoded;
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Oops something get wrong, try again !' });
   }
-}
+};
 
 const isNotAuthenticated = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
@@ -59,4 +79,7 @@ const isNotAuthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { isAuthenticated, isNotAuthenticated };
+module.exports = {
+  isAuthenticated,
+  isNotAuthenticated,
+};
