fa181f2aa0f89bbe298819845ffdcb4a854cdc8c
gary-j <gary.j@live.fr>
2022-06-08 02:47:10 +0200

    test origins array écrit en dure

--- config/index.js
+++ config/index.js
@@ -23,7 +23,11 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || 'http://localhost:3000',
+      origin: [
+        'https://citron-en-limonade.vercel.app',
+        'https://citronenlimonade.com',
+        'https://citronenlimonade.fr',
+      ],
     })
   );
 
