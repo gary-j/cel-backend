b22600127f2a5be3aab8c62c6739f3d8f97d5729
gary-j <gary.j@live.fr>
2022-07-19 11:08:03 +0200

    add populate writter theme and professional in route get all stories

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const Story = require('../models/Story.model');
+const Professional = require('../models/Professional.model');
+// Ajout du model Professional et le .populate fonctionne pour lui
+// mais pour Theme.model et User.model bizarrement ça fonctionne sans import du schema
 //
 const stories_get = async (req, res, next) => {
-  const stories = await Story.find().sort({ createdAt: -1 }).limit(10);
+  const stories = await Story.find()
+    .populate('writter theme professionalConsulted')
+    .sort({ createdAt: -1 })
+    .limit(10);
   console.log('stories_get, toutes les stories', stories);
   res.status(200).json(stories);
 };
