7835c9d4107daadd0743a8e79e263197c65a961c
gary-j <gary.j@live.fr>
2022-08-11 00:33:00 +0200

    create route and controller to  post a story

--- routes/story.routes.js
+++ routes/story.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const storyController = require('../controllers/storyController');
 //
 router.get('/', storyController.stories_get);
+router.post('/create', storyController.createStory_post);
 
 module.exports = router;
