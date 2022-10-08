bc3803b051aca95364f23a7f20172ba4382f11f8
gary-j <gary.j@live.fr>
2022-10-09 00:33:33 +0200

    create route to get bodyParts from db

---
+++ controllers/bodyPartController.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const BodyPart = require('../models/BodyPart.model');
+//
+const bodyparts_get = async (req, res, next) => {
+  try {
+    const bodyParts = await BodyPart.find();
+    // console.log('les bodyparts : ', bodyParts);
+    res.status(200).json(bodyParts);
+  } catch (error) {
+    console.log(error, 'error fetch body parts');
+    next(error);
+    return;
+  }
+};
+
+module.exports = {
+  bodyparts_get,
+};
