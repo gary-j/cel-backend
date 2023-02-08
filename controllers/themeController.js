1dcddf34be422a88d2ddcbb62fd4432fabfc3d77
gary-j <gary.j@live.fr>
2023-02-07 20:49:45 -0400

    creat route get one theme ok

--- controllers/themeController.js
+++ controllers/themeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Theme = require('../models/Theme.model');
+const mongoose = require('mongoose');
 
 const theme_get = async (req, res, next) => {
   // console.log('appel ok theme_get');
@@ -12,6 +13,32 @@ const theme_get = async (req, res, next) => {
   } catch (error) {}
 };
 
+// GET Theme By id or slug !
+
+const one_theme_get = async (req, res, next) => {
+  const query = req.params.idOrSlug;
+  console.log('la query: ', query);
+  try {
+    if (mongoose.isValidObjectId(query)) {
+      console.log('try by id');
+      const theme = await Theme.findById(query);
+      if (theme === null) {
+        const theme = await Theme.findOne({ slug: query });
+        res.status(200).json(theme);
+        return;
+      }
+      res.json(theme);
+    } else {
+      console.log('try by slug');
+      const theme = await Theme.findOne({ slug: query });
+      res.status(200).json(theme);
+    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
 module.exports = {
   theme_get,
+  one_theme_get,
 };
--- routes/theme.routes.js
+++ routes/theme.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const themeController = require('../controllers/themeController');
 router.get('/all', themeController.theme_get);
+router.get('/:idOrSlug', themeController.one_theme_get);
 
 module.exports = router;
